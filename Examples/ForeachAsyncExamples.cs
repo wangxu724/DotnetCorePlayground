7c69368c1aafc851b7dfdc20c5abc96a6d449804
Xu Wang <wangxu724@gmail.com>
2020-03-21 01:15:52 -0700

    Demo for foreach + async behaviors

---
+++ Examples/ForeachAsyncExamples.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotnetCorePlayground.Examples
+{
+    public class ForeachAsyncExamples
+    {
+        public ForeachAsyncExamples()
+        {
+        }
+
+        public async Task Run()
+        {
+            int[] nums = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("This section shows foreach+async runs sequentially.");
+            foreach (var num in nums)
+            {
+                Console.WriteLine($"num: {num}");
+                var rlt = await getRandomNumberAsync();
+                Console.WriteLine($"num: {num}; rlt: {rlt}");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("This section shows foreach+async+break will quit following runs.");
+            foreach (var num in nums)
+            {
+                Console.WriteLine($"num: {num}");
+                var rlt = await getRandomNumberAsync();
+                Console.WriteLine($"num: {num}; rlt: {rlt}");
+                if (rlt % 2 == 0)
+                {
+                    Console.WriteLine($"Break on {rlt}");
+                    break;
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("This section shows foreach+async+exception-within-async will quit following runs.");
+            try
+            {
+                foreach (var num in nums)
+                {
+                    Console.WriteLine($"num: {num}");
+                    var rlt = await getRandomThrowAsync();
+                    Console.WriteLine($"num: {num}; rlt: {rlt}");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+        }
+
+        private static Task<int> getRandomNumberAsync()
+        {
+            return Task<int>.Run(() =>
+            {
+                var rand = new Random();
+                Thread.Sleep(1000);
+                var rlt = rand.Next(0, 10);
+                return rlt;
+            });
+        }
+
+        private static Task<int> getRandomThrowAsync()
+        {
+            return Task<int>.Run(() =>
+            {
+                var rand = new Random();
+                Thread.Sleep(1000);
+                var rlt = rand.Next(0, 10);
+                if (rlt % 2 == 0)
+                {
+                    throw new Exception("Got even number;");
+                }
+                return rlt;
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DotnetCorePlayground.Examples;
 
 namespace DotnetCorePlayground
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to .net core playground!\n");
+
+            var foreachAsyncExammple = new ForeachAsyncExamples();
+            await foreachAsyncExammple.Run();
         }
     }
 }
