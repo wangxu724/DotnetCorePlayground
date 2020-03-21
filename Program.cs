55adb524a51e94a13afe6a4626ee0f6a507a208f
Xu Wang <wangxu724@gmail.com>
2020-03-21 01:06:20 -0700

    Add empty project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetCorePlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
